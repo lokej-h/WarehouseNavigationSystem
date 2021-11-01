d6bcf123e7045652e2e390e7417678221773cc47
Vikas Naveen <vkas15@gmail.com>
2021-11-01 14:07:26 -0700

    adding ability to change warehouse file dynamically

--- WNS/Controller/warehouse_controller.py
+++ WNS/Controller/warehouse_controller.py
@@ -64,3 +64,54 @@ def get_warehouse_shelves() -> Dict[str, Tuple[int, int]]:
             # around the border of the warehouse
             product_dict[l[0]] = (x + 1, y + 1)
         return product_dict
+
+
+def change_warehouse_shelves(file_path) -> Dict[str, Tuple[int, int]]:
+    """
+    Change warehouse data and read from user specified file.
+    
+    This program assumes that the file provided matches the specification:
+        
+    1. The first line must be the column names.
+    2. Every consecutive line must have three numbers separated by whitespace
+    in the order "ProductID, xLocation, yLocation"
+    
+    Example:
+    
+    Product 1 is at location (5,8)
+    
+        ProductID    xLocation    yLocation
+        1 5 8 
+
+    Returns
+    -------
+    Dict[str, List[Tuple[int, int]]]
+    
+    Returns a dictionary of items and corresponding shelves.
+        
+    Key: PID as string
+        
+    Value: (x,y) coordinate of item's shelf
+    
+    This means that dict.values() will have duplicates.
+
+    """
+    with open(file_path) as file:
+        # read out column names from buffer
+        file.readline().split()
+
+        product_dict = dict()
+
+        for line in file.readlines():
+            l = line.split()
+            x = int(float(l[1]))
+            y = int(float(l[2]))
+            # the coordinates start at 0,0
+            # this 0,0 transalates to 2,B or 2,2
+            # we will tackle in two steps
+            # +1 during load, we +1 for calculations (empty space in 0th row col)
+            # +1 for view coordinates for user
+            # add 1 to each coordinate to add the space for the walkway
+            # around the border of the warehouse
+            product_dict[l[0]] = (x + 1, y + 1)
+        return product_dict
